c9ccd054be9e0d5976ba4ef0507bfdc10a3bfc9f
Gnezd <ycdzeng@gmail.com>
2022-07-23 16:42:10 +0800

    Visual tweaks

--- symbol_tables/generate.rb
+++ symbol_tables/generate.rb
@@ -25,8 +25,8 @@ start_row = 0x390
 
 (0..3).each do |row_num|
   row = start_row + 16* row_num
-  line_unicode = "<tr class=\"unicode\" id=\"row#{row_num}-unicode\"><td>" + ((0..15).map {|i| "%04x" % (i + row)}).join("</td><td>") + "</tr>"
-  line_letter = "<tr class=\"letter\" id=\"row#{row_num}-letter\"><td>" + ((0..15).map {|i| "%c" % (i + row)}).join("</td><td>") + "</tr>"
+  line_unicode = "<tr class=\"unicode\" id=\"row#{row_num}-unicode\">" + ((0..15).map {|i| "<td id=\"x#{row_num}y#{i}\"> %04x </td>" % (i + row)}).join("\n") + "</tr>"
+  line_letter = "<tr class=\"letter\" id=\"row#{row_num}-letter\">" + ((0..15).map {|i| "<td id=\"x#{row_num}y#{i}\"> %c </td>" % (i + row)}).join("\n") + "</tr>"
   html_out.puts line_unicode
   html_out.puts line_letter
 end
