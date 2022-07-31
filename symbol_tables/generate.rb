0543d5ab4bfb81e7013ec760ab0d11594ccc4613
Gnezd <ycdzeng@gmail.com>
2022-07-31 22:59:51 +0800

    adding Ø

--- symbol_tables/generate.rb
+++ symbol_tables/generate.rb
@@ -1,13 +1,13 @@
 #mappings = (0x0390 .. 0x03cf).map {|code| ["%04x" % code, "%c" % code]}
 #puts mappings
 
-html_out = File.open 'greek_letters.html', 'w'
+html_out = File.open 'greek_letters_and_some_other_symbols.html', 'w'
 
 html_head = <<HTMLHEAD
 <!DOCTYPE html>
 <html>
 <head>
-<title>Greek letters</title>
+<title>Greek letters and some other symbols</title>
 <link rel="stylesheet" href="./style.css">
 </head>
 <body>
@@ -22,7 +22,6 @@ html_out.puts html_head
 html_out.puts "<table>"
 
 start_row = 0x390
-
 (0..3).each do |row_num|
   row = start_row + 16* row_num
   line_unicode = "<tr class=\"unicode\" id=\"row#{row_num}-unicode\">" + ((0..15).map {|i| "<td id=\"x#{row_num}y#{i}\"> %04x </td>" % (i + row)}).join("\n") + "</tr>"
@@ -30,6 +29,12 @@ start_row = 0x390
   html_out.puts line_unicode
   html_out.puts line_letter
 end
+
+some_others = [0x00b0, 0x2026, 0xd8]
+html_out.puts "<tr><td></td></tr>"
+html_out.puts "<tr class=\"unicode\" id=\"row-1-unicode\">" + ((0..some_others.size-1).map {|i| "<td id=\"x-1y#{i}\"> %04x </td>" % (some_others[i])}).join("\n") + "</tr>"
+html_out.puts "<tr class=\"letter\" id=\"row-1-letter\">" + ((0..some_others.size-1).map {|i| "<td id=\"x-1y#{i}\"> %c </td>" % (some_others[i])}).join("\n") + "</tr>"
+
 
 html_out.puts "</table>"
 
